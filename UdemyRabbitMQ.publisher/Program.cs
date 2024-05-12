f2b52670956631a0450f57feb15819dbaefd182e
Eren Çolak <eren.colk01@gmail.com>
2024-05-12 21:10:07 +0300

    DirectExchange

--- UdemyRabbitMQ.publisher/Program.cs
+++ UdemyRabbitMQ.publisher/Program.cs
@@ -5,6 +5,18 @@ namespace UdemyRabbitMQ.publisher
 {
     internal class Program
     {
+
+        public enum LogNames
+        {
+            Ciritical = 1,
+            Error = 2,
+            Warning = 3,
+            Info = 4,
+
+        }
+
+
+
         static void Main(string[] args)
         {
 
@@ -42,33 +54,84 @@ namespace UdemyRabbitMQ.publisher
 
 
 
+            //FANOUT EXCHANGE
+            ////KUYRUGU PUBLİSHER OLARAK OLUSTURMAMA. 
+
+            //var factory = new ConnectionFactory();
+
+            //factory.Uri = new Uri("amqps://cezgimih:Xo_fII9ov60oS1bqMUETJEKjNyAK-JC3@toad.rmq.cloudamqp.com/cezgimih");
+
+            //using var connection = factory.CreateConnection();
+
+            //var channel = connection.CreateModel();
+
+            //channel.ExchangeDeclare("logs-fanout",durable:true,type:ExchangeType.Fanout);  // Exchange olusturuyoruz. 1.paramatre exchange adı. 2.parametre uygulama restartta atsa exchange kaybolmasın.
+
+            //Enumerable.Range(1, 50).ToList().ForEach(x =>
+            //{
+            //    string message = $"log {x}";
+
+            //    var messageBody = Encoding.UTF8.GetBytes(message);
+            //    channel.BasicPublish("logs-fanout","", null, messageBody); //1.parametrede exchange'in adını veriyoruz. 2.parametrede kuyrugu publisher olusturmayacağı için boş bırakıyoruz.
+
+            //    Console.WriteLine($"Mesaj gönderilmiştir. : {message}");
+            //});
 
 
-            //KUYRUGU PUBLİSHER OLARAK OLUSTURMAMA. FANOUT EXCHANGE
 
-            var factory = new ConnectionFactory();
+            //Console.ReadLine();
 
-            factory.Uri = new Uri("amqps://cezgimih:Xo_fII9ov60oS1bqMUETJEKjNyAK-JC3@toad.rmq.cloudamqp.com/cezgimih");
 
-            using var connection = factory.CreateConnection();
 
-            var channel = connection.CreateModel();
 
-            channel.ExchangeDeclare("logs-fanout",durable:true,type:ExchangeType.Fanout);  // Exchange olusturuyoruz. 1.paramatre exchange adı. 2.parametre uygulama restartta atsa exchange kaybolmasın.
 
+
+            //DIRECT EXCHANGE
+            //KUYRUGU PUBLİSHER OLARAK OLUSTURMAMA.
+
+            var factory = new ConnectionFactory(); // RabbitMQ bağlantı fabrikası oluşturuluyor.
+
+            factory.Uri = new Uri("amqps://cezgimih:Xo_fII9ov60oS1bqMUETJEKjNyAK-JC3@toad.rmq.cloudamqp.com/cezgimih"); // RabbitMQ bağlantı adresi belirleniyor.
+
+            using var connection = factory.CreateConnection(); // Bağlantı oluşturuluyor ve "using" bloğu içinde otomatik olarak temizleniyor.
+
+            var channel = connection.CreateModel(); // Kanal (channel) oluşturuluyor.
+
+            channel.ExchangeDeclare("logs-direct", durable: true, type: ExchangeType.Direct); // Direct tipinde bir exchange oluşturuluyor.
+
+
+            Enum.GetNames(typeof(LogNames)).ToList().ForEach(x =>
+            {
+                // Log türlerine göre kuyruk (queue) ve bağlantılar (bindings) tanımlanıyor.
+                var routeKey = $"route-{x}"; 
+                var queueName = $"direct-queue-{x}";
+                channel.QueueDeclare(queueName, true, false, false);
+                channel.QueueBind(queueName, "logs-direct", routeKey, null);
+            });
+
+            // Rastgele log mesajları yayınlamak için bir döngü oluşturuluyor.
             Enumerable.Range(1, 50).ToList().ForEach(x =>
             {
-                string message = $"log {x}";
+                LogNames log = (LogNames)new Random().Next(1, 5); // Rastgele bir log türü seçiliyor.
 
-                var messageBody = Encoding.UTF8.GetBytes(message);
-                channel.BasicPublish("logs-fanout","", null, messageBody); //1.parametrede exchange'in adını veriyoruz. 2.parametrede kuyrugu publisher olusturmayacağı için boş bırakıyoruz.
+                string message = $"log-type : {log}"; // Log mesajı oluşturuluyor.
 
-                Console.WriteLine($"Mesaj gönderilmiştir. : {message}");
+                var messageBody = Encoding.UTF8.GetBytes(message); // Mesaj byte dizisine dönüştürülüyor.
+
+                var routeKey = $"route-{log}"; // Log türüne göre routeKey belirleniyor.
+
+                // Log mesajı yayınlanıyor.
+                channel.BasicPublish("logs-direct", routeKey, null, messageBody);
+
+                Console.WriteLine($"Log gönderilmiştir. : {message}"); // Yayınlanan log mesajı konsola yazdırılıyor.
             });
 
 
-
             Console.ReadLine();
+
+
+
+
 
         }
     }
--- UdemyRabbitMQ.subcriber/Program.cs
+++ UdemyRabbitMQ.subcriber/Program.cs
@@ -6,6 +6,7 @@ namespace UdemyRabbitMQ.subcriber
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
 
@@ -46,8 +47,59 @@ namespace UdemyRabbitMQ.subcriber
 
 
 
+            //FANOUT EXCHANGE
+            //KUYRUGU PUBLİSHER OLARAK OLUSTURMAMA. SUCRIBER TARAFINDA OLUSTURULUR. KUYRUGUN ISMI ÖNEMLİ DEĞİLDİR.
 
-            //KUYRUGU PUBLİSHER OLARAK OLUSTURMAMA. FANOUT EXCHANGE
+            //var factory = new ConnectionFactory();
+
+            //factory.Uri = new Uri("amqps://cezgimih:Xo_fII9ov60oS1bqMUETJEKjNyAK-JC3@toad.rmq.cloudamqp.com/cezgimih");
+
+
+            //using var connection = factory.CreateConnection();
+
+
+            //var channel = connection.CreateModel();
+
+            ////publisher tarafıdna olusturdugumuz için burada olusturmamıza gerek yok. O yüzden yoruma alındı
+            ////channel.ExchangeDeclare("logs-fanout", durable: true, type: ExchangeType.Fanout);  // Exchange olusturuyoruz. 1.paramatre exchange adı. 2.parametre uygulama restartta atsa exchange kaybolmasın.
+
+            //var randomQueueName = "log-database-save-queue"; /*channel.QueueDeclare().QueueName;*/ //random kuyruk ismi oluşturmaya yarıyor.
+
+
+            //channel.QueueDeclare(randomQueueName, true, false, false); // 1. parametre kuyruk ismi. 2.parametre fiziksel olarak sabit diskte kaydedilsin. 3. parametre başka kanallardan baglanılsınmı. 4.parametre down olunursa kuyruk silinsinmi
+
+            //channel.QueueBind(randomQueueName, "logs-fanout", "", null); // Kuyruk declare etmek yerine channel üstünden kuyrugu exchange baglıyoruz. Bunun sebebi Consumer yani subcriber çıkış yaptıgında kuyrukta silinsin
+
+
+
+            //channel.BasicQos(0, 1, false); // 1.parametre boyut herhangi bir boyuttaki mesajı gönderebilirsin. 2.parametre kaç kaç mesaj gelsin. 3.parametre değer global olsunmu yani her bir subcribera eğer 2. parametre 6 seçildiyse ve 3.parametre true ise herbir sucribera 2 ser 2ser gönderir. Eğer false ise herbir sucribera 6'sar 6'sar gönderir.
+
+            //var consumer = new EventingBasicConsumer(channel);
+
+            //channel.BasicConsume(randomQueueName, false, consumer); // dinleyeceği kuyruğu belirtiyoruz. // ikinci parametre true yaparsan rabbitmq kuyruktan bir mesaj gönderdiğinde yanlısda dogruda gönderse siler, false yaparsan silmez
+
+            //Console.WriteLine("Loglar Dinleniyor");
+
+            //consumer.Received += (object? sender, BasicDeliverEventArgs e) =>
+            //{  //RabbitMq subcribera mesaj gönderdiğinde buradaki event fırlar ve burada yakalarız.
+            //    var message = Encoding.UTF8.GetString(e.Body.ToArray());
+
+            //    Console.WriteLine("Gelen Mesaj : " + message);
+
+            //    channel.BasicAck(e.DeliveryTag, false); //işlem başarılıysa işlemi siliyor. // 2. parametre memoryde işlenmiş ama rabbitmq gitmemiş mesajları rabbitmq a haberdar eder.
+            //};
+
+            //Console.ReadLine();
+
+
+
+
+
+
+
+
+
+            //DIRECT EXCHANGE
 
             var factory = new ConnectionFactory();
 
@@ -62,20 +114,14 @@ namespace UdemyRabbitMQ.subcriber
             //publisher tarafıdna olusturdugumuz için burada olusturmamıza gerek yok. O yüzden yoruma alındı
             //channel.ExchangeDeclare("logs-fanout", durable: true, type: ExchangeType.Fanout);  // Exchange olusturuyoruz. 1.paramatre exchange adı. 2.parametre uygulama restartta atsa exchange kaybolmasın.
 
-            var randomQueueName = "log-database-save-queue"; /*channel.QueueDeclare().QueueName;*/ //random kuyruk ismi oluşturmaya yarıyor.
-
-
-            channel.QueueDeclare(randomQueueName, true, false, false); // 1. parametre kuyruk ismi. 2.parametre fiziksel olarak sabit diskte kaydedilsin. 3. parametre başka kanallardan baglanılsınmı. 4.parametre down olunursa kuyruk silinsinmi
-
-            channel.QueueBind(randomQueueName, "logs-fanout", "", null); // Kuyruk declare etmek yerine channel üstünden kuyrugu exchange baglıyoruz. Bunun sebebi Consumer yani subcriber çıkış yaptıgında kuyrukta silinsin
-
-
 
             channel.BasicQos(0, 1, false); // 1.parametre boyut herhangi bir boyuttaki mesajı gönderebilirsin. 2.parametre kaç kaç mesaj gelsin. 3.parametre değer global olsunmu yani her bir subcribera eğer 2. parametre 6 seçildiyse ve 3.parametre true ise herbir sucribera 2 ser 2ser gönderir. Eğer false ise herbir sucribera 6'sar 6'sar gönderir.
 
             var consumer = new EventingBasicConsumer(channel);
 
-            channel.BasicConsume(randomQueueName, false, consumer); // dinleyeceği kuyruğu belirtiyoruz. // ikinci parametre true yaparsan rabbitmq kuyruktan bir mesaj gönderdiğinde yanlısda dogruda gönderse siler, false yaparsan silmez
+            var queueName = "direct-queue-Ciritical";
+
+            channel.BasicConsume(queueName, false, consumer); // dinleyeceği kuyruğu belirtiyoruz. // ikinci parametre true yaparsan rabbitmq kuyruktan bir mesaj gönderdiğinde yanlısda dogruda gönderse siler, false yaparsan silmez
 
             Console.WriteLine("Loglar Dinleniyor");
 
@@ -83,12 +129,19 @@ namespace UdemyRabbitMQ.subcriber
             {  //RabbitMq subcribera mesaj gönderdiğinde buradaki event fırlar ve burada yakalarız.
                 var message = Encoding.UTF8.GetString(e.Body.ToArray());
 
+                Thread.Sleep(1000);
                 Console.WriteLine("Gelen Mesaj : " + message);
+
+                //File.AppendAllText("log-critical.txt", message + "\n");
 
                 channel.BasicAck(e.DeliveryTag, false); //işlem başarılıysa işlemi siliyor. // 2. parametre memoryde işlenmiş ama rabbitmq gitmemiş mesajları rabbitmq a haberdar eder.
             };
 
             Console.ReadLine();
+
+
+
+
         }
 
 
