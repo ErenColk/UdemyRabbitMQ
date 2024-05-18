421d918371cfad63c3d4745e68869243c934f6d3
Eren Çolak <eren.colk01@gmail.com>
2024-05-18 20:04:53 +0300

    Userfile oluşturma

--- UdemyRabbitMQWeb.ExcelCreate/Migrations/20240518162337_filepath_null.Designer.cs
+++ UdemyRabbitMQWeb.ExcelCreate/Migrations/20240518162337_filepath_null.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using UdemyRabbitMQWeb.ExcelCreate.Models;
 
@@ -11,9 +12,10 @@ using UdemyRabbitMQWeb.ExcelCreate.Models;
 namespace UdemyRabbitMQWeb.ExcelCreate.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240518162337_filepath_null")]
+    partial class filepath_null
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
