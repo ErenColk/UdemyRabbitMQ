b09d03f2df8cdaf3912f569367007248fe20fb59
Eren Çolak <eren.colk01@gmail.com>
2024-05-18 22:07:35 +0300

    File için API oluşturuldu

---
+++ UdemyRabbitMQWeb.ExcelCreate/Controllers/FilesController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UdemyRabbitMQWeb.ExcelCreate.Models;
+
+namespace UdemyRabbitMQWeb.ExcelCreate.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilesController : ControllerBase
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public FilesController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+        public async Task<IActionResult> Upload(IFormFile file, int fileId)
+        {
+            if (file is not { Length: > 0 }) return BadRequest();
+
+            var userFile = await _appDbContext.UserFiles.FirstAsync(x => x.Id == fileId);
+
+            var filePath = userFile.FileName + Path.GetExtension(file.FileName);
+
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/files", filePath);
+
+            using FileStream stream = new(path, FileMode.Create); //Dosyayı kaydeder
+
+            await file.CopyToAsync(stream); //Dosyanın içerğini belirtir.
+
+
+            userFile.CreatedDate = DateTime.Now;
+            userFile.FilePath = filePath;
+            userFile.FileStatus = FileStatus.Completed;
+
+            await _appDbContext.SaveChangesAsync();
+
+            //SignalR notification oluşturulacak
+
+            return Ok();    
+        }
+
+    }
+}
